fc8cc912c07af046d74468533225a461a99ad7ca
LTrain <leonp_ms@hotmail.com>
2023-10-31 16:33:54 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023midet",
+      "defaultValue": "2023mibg",
       "required": "true",
       "disabled": "true"
     },
